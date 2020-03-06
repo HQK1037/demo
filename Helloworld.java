2c17e5cf2df60d26d9823afb83f718541b4ef854
HQK1037 <HQK1037@gmail.com>
2020-03-06 13:52:33 +0800

    helloworld2

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public static Helloworld{
+	public static void main(String args[]){
+		system.out.printlin("helloworld");
+		system.out.printlin("1");
+		system.out.printlin("2");
+		system.out.printlin("helloworld");
+	
+	}
+}
